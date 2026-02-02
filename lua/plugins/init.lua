168d46cc8a86f9d9d03b2aab78738ea6de009aec
Retr01-sudo <retrowrench.06@gmail.com>
2026-02-03 00:37:24 +0530

    Added folds feature

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -76,4 +76,32 @@ return {
             require("configs.lspconfig")
         end,
     },
+
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        config = function()
+            vim.o.foldcolumn = "1"
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            -- Set foldmethod to use ufo
+            vim.o.foldmethod = "expr"
+            vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+            -- Configure ufo
+            require("ufo").setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return { "treesitter", "indent" }
+                end,
+            })
+
+            -- Keymaps for folding
+            vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+            vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+        end,
+    },
 }
